bc055a1639123bcd6e18c53c0d23713c00f22ef7
jorgen saeter <jorgen.sater@gmail.com>
2021-11-15 21:56:47 +1100

    Added more test cases for renamer.get_path

--- test/test_renamer.py
+++ test/test_renamer.py
@@ -2,13 +2,14 @@ import pytest
 import os
 import sys
 sys.path.append('.')
-import r
+import renamer
 
 @pytest.mark.parametrize("path, expected", [
     ('.', os.getcwd()),
     ('C:', 'C:\\'),
     ('C:\\ProgramData', 'C:\\ProgramData'),
+    ('test\\test_renamer.py', 'D:\\Code\\utility\\test\\test_renamer.py'),
 ])
 
 def test_get_path(path, expected):
-    assert(r.get_path(path)) == expected
+    assert(renamer.get_path(path)) == expected
